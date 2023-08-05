22de0c4f35b01d9b6896fe6dd7b19de0a2910918
Ramil Mavliutov <stirk2281337@gmail.com>
2023-08-05 16:37:46 +0500

    drf JWT TOKENS( access, refresh)

--- drfblog/urls.py
+++ drfblog/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from blog.views import *
 
@@ -21,4 +22,7 @@ urlpatterns = [
     path('api/v1/blogdelete/<int:pk>/', BlogAPIDestroy.as_view()),
     path('api/v1/auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
